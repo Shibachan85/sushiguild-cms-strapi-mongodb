b8f7b980e2df0830d551aed3b94def985974862e
Jesper Bengtsson Ekwall <jeppen@live.com>
2021-05-30 11:22:43 +0200

    inital commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c0273ab031e657ce1c900895f3d947c'),
+    },
+  },
+});
